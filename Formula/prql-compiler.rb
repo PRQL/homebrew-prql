8272267de827fbafa446cc1c1f5d911c3dc13b63
roG0d <rodgarcas98@gmail.com>
2022-06-27 10:24:13 +0200

    MODIFIED: prql version to prerelease 0.1.2

--- Formula/prql-compiler.rb
+++ Formula/prql-compiler.rb
@@ -1,16 +1,16 @@
 class PrqlCompiler < Formula
   desc "PRQL is a modern language for transforming data — a simpler and more powerful SQL"
   homepage "https://prql.github.io/prql-website/"
-  url "https://github.com/prql/prql/archive/refs/tags/0.1.1.tar.gz"
+  url "https://github.com/prql/prql/archive/refs/tags/0.1.2.tar.gz"
   sha256 "ab4a4d1f3082d874d510d4ded9e2665dd6530fa8c209aa6231fbd713ae1cc8be"
-  version "0.1.1"
+  version "0.1.2"
   license "Apache-2.0"
   
   depends_on "rust" => :build
 
   def install
     system "cargo", "build", "--release", "--bin", "prql-compiler"
-    bin.install "target/release/prql"
+    bin.install "target/release/prql-compiler"
   end
 
   test do
