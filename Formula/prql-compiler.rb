0ddb323e1fa72b2929dc15efbafa19c05455d4a7
roG0d <rodgarcas98@gmail.com>
2022-06-27 10:28:48 +0200

    MODIFIED: sha256 prql-compiler

--- Formula/prql-compiler.rb
+++ Formula/prql-compiler.rb
@@ -2,7 +2,7 @@ class PrqlCompiler < Formula
   desc "PRQL is a modern language for transforming data — a simpler and more powerful SQL"
   homepage "https://prql.github.io/prql-website/"
   url "https://github.com/prql/prql/archive/refs/tags/0.1.2.tar.gz"
-  sha256 "ab4a4d1f3082d874d510d4ded9e2665dd6530fa8c209aa6231fbd713ae1cc8be"
+  sha256 "4abdee53e3f938ebc1c27be08e3de747738606c4aeaef5771d562d903930617c"
   version "0.1.2"
   license "Apache-2.0"
   
