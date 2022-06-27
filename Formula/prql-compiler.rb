3978018e63fd6660e60bd7a0a1d32a1655997bae
Rodrigo Garcia <32329949+roG0d@users.noreply.github.com>
2022-06-27 12:32:28 +0200

    Testing fields facing the release

--- Formula/prql-compiler.rb
+++ Formula/prql-compiler.rb
@@ -1,9 +1,9 @@
 class PrqlCompiler < Formula
   desc "PRQL is a modern language for transforming data — a simpler and more powerful SQL"
   homepage "https://prql.github.io/prql-website/"
-  url "https://github.com/prql/prql/archive/refs/tags/0.1.2.tar.gz"
-  sha256 "4abdee53e3f938ebc1c27be08e3de747738606c4aeaef5771d562d903930617c"
-  version "0.1.2"
+  url "testing param for the release"
+  sha256 "testing param for the release"
+  version "testing param for the release"
   license "Apache-2.0"
   
   depends_on "rust" => :build
