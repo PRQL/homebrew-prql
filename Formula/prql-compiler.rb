986061b3d9181b4214cb17770f7069f825ff66e5
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2022-07-18 22:34:02 -0700

    Update website location

--- Formula/prql-compiler.rb
+++ Formula/prql-compiler.rb
@@ -1,6 +1,6 @@
 class PrqlCompiler < Formula
   desc "PRQL is a modern language for transforming data — a simpler and more powerful SQL"
-  homepage "https://prql.github.io/prql-website/"
+  homepage "https://prql-lang.org"
   url "https://github.com/prql/prql/archive/refs/tags/0.2.2.tar.gz"
   sha256 "c23d30611b4851f8d62297eca8e3851b16772548dfde00ca2df23e17e8264cd0"
   version "0.2.2"
