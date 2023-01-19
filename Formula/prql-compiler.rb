320f3632fcf78a51065b5fcea45952c07a2a4ef0
Laine Taffin Altman <alexanderaltman@me.com>
2023-01-19 10:45:10 -0800

    Fix and format both formulae
    They now comply, at least nominally, with Homebrew standard style and recommendations.
    
    This includes removing the explicit version line, since it's inferred from the URL successfully.  That required changes to the GitHub Action YAMLs.

--- Formula/prql-compiler.rb
+++ Formula/prql-compiler.rb
@@ -1,19 +1,17 @@
 class PrqlCompiler < Formula
-  desc "PRQL is a modern language for transforming data — a simpler and more powerful SQL"
+  desc "PRQL is a modern language for transforming data: a simpler and more powerful SQL"
   homepage "https://prql-lang.org"
   url "https://github.com/PRQL/prql/archive/refs/tags/0.4.1.tar.gz"
   sha256 "b60df362a0b823600ccc614c335f4a8876a577a7ceeb6d162f16540fd9340f73"
-  version "0.4.1"
   license "Apache-2.0"
-  
+
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build", "--release", "--bin", "prql-compiler"
-    bin.install "target/release/prql-compiler"
+    system "cargo", "install", *std_cargo_args(path: "prql-compiler/prqlc")
   end
 
   test do
-   
+    system bin/"prqlc", "-h"
   end
 end
--- Formula/prql-query.rb
+++ Formula/prql-query.rb
@@ -3,17 +3,15 @@ class PrqlQuery < Formula
   homepage "https://github.com/prql/prql-query"
   url "https://github.com/prql/prql-query/archive/refs/tags/v0.0.14.tar.gz"
   sha256 "4b1ab070dacd25da3361edeb2a0728c4bedec0e9620105580072520eeb2ea5cd"
-  version "v0.0.14"
   license "Apache-2.0"
-  
+
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build", "--release", "--bin", "pq"
-    bin.install "target/release/pq"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
-   
+    system bin/"pq", "-h"
   end
 end
