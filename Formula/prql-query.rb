c09d429d2c4d3c5e98c0a04b3051862206b8598e
Laine Taffin Altman <alexanderaltman@me.com>
2023-01-18 22:44:39 -0800

    prql-query v0.0.14

--- Formula/prql-query.rb
+++ Formula/prql-query.rb
@@ -1,9 +1,9 @@
 class PrqlQuery < Formula
   desc "Query and transform data with PRQL"
   homepage "https://github.com/prql/prql-query"
-  url "https://github.com/prql/prql-query/archive/refs/tags/v0.0.12.tar.gz"
-  sha256 "970a441d3ed413782d4e801cf9c10d67bac7c4c826f2c0bbf925b9ee2237e216"
-  version "v0.0.12"
+  url "https://github.com/prql/prql-query/archive/refs/tags/v0.0.14.tar.gz"
+  sha256 "4b1ab070dacd25da3361edeb2a0728c4bedec0e9620105580072520eeb2ea5cd"
+  version "v0.0.14"
   license "Apache-2.0"
   
   depends_on "rust" => :build
